8052ce0cdff3c60b2a200ba661ce0e16cfb6846c
Persephone <its.persephone.xo@gmail.com>
2023-12-31 16:04:40 -0500

    attempt to fix

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        // More information here https://webpack.js.org/guides/asset-modules/
+        type: "asset",
+      },
     ],
   },
 };
